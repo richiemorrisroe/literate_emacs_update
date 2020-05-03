f7cc86c36a35cfb45d5c88e483538b41105fa816
richie <richie.morrisroe@gmail.com>
2020-05-03 15:49:26 +0100

    add base-pkg file
    this is minimal stuff to exist in emacs
    use-package and magit

---
+++ base-pkg.el
@@ -0,0 +1,16 @@
+(unless (package-installed-p 'use-package)
+	(package-refresh-contents)
+	(package-install 'use-package))
+
+	(unless (package-installed-p 'diminish)
+	(package-refresh-contents)
+	(package-install 'diminish))
+      (use-package magit :ensure t)
+  (setq package-archives '(("elpa" . "https://tromey.com/elpa/")
+			   ("gnu" . "https://elpa.gnu.org/packages/")
+			   ("marmalade" . "https://marmalade-repo.org/packages/")))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives
+'("melpa-stable" . "https://stable.melpa.org/packages/"))
+;; (add-to-list 'package-archives
+;; '("melpa" . "https://melpa.org/packages/"))
