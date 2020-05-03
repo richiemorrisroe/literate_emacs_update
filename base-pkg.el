973997fb0a36b15680994a7447a9e54cb0471aa4
richie <richie.morrisroe@gmail.com>
2020-05-03 16:02:31 +0100

    add wheatgrass theme to base-pkg

--- base-pkg.el
+++ base-pkg.el
@@ -1,20 +1,21 @@
 (unless (package-installed-p 'use-package)
-	(package-refresh-contents)
-	(package-install 'use-package))
+	  (package-refresh-contents)
+	  (package-install 'use-package))
 
-	(unless (package-installed-p 'diminish)
-	(package-refresh-contents)
-	(package-install 'diminish))
-      (use-package magit :ensure t)
-  (setq package-archives '(("elpa" . "https://tromey.com/elpa/")
-			   ("gnu" . "https://elpa.gnu.org/packages/")
-			   ("marmalade" . "https://marmalade-repo.org/packages/")))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives
-'("melpa-stable" . "https://stable.melpa.org/packages/"))
-;; (add-to-list 'package-archives
-;; '("melpa" . "https://melpa.org/packages/"))
-(global-set-key "\C-cg" 'magit-status)
-(global-set-key (kbd "C-x m") 'execute-extended-command)
-  (use-package helpful
-    :ensure t)
+	  (unless (package-installed-p 'diminish)
+	  (package-refresh-contents)
+	  (package-install 'diminish))
+	(use-package magit :ensure t)
+    (setq package-archives '(("elpa" . "https://tromey.com/elpa/")
+			     ("gnu" . "https://elpa.gnu.org/packages/")
+			     ("marmalade" . "https://marmalade-repo.org/packages/")))
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives
+  '("melpa-stable" . "https://stable.melpa.org/packages/"))
+  ;; (add-to-list 'package-archives
+  ;; '("melpa" . "https://melpa.org/packages/"))
+  (global-set-key "\C-cg" 'magit-status)
+  (global-set-key (kbd "C-x m") 'execute-extended-command)
+    (use-package helpful
+      :ensure t)
+(load-theme 'wheatgrass t)
